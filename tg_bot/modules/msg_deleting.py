f2210f4bb7cb970fae39d2f5996dd353d859c606
Sixteenbit <76763839+SixteenByte@users.noreply.github.com>
2021-01-09 18:57:49 +0530

    Update msg_deleting.py

--- tg_bot/modules/msg_deleting.py
+++ tg_bot/modules/msg_deleting.py
@@ -9,12 +9,12 @@ from telegram.utils.helpers import mention_html
 
 from tg_bot import dispatcher, LOGGER
 from tg_bot.modules.helper_funcs.chat_status import user_admin, can_delete
-from tg_bot.modules.log_channel import loggable
+
 
 
 @run_async
 @user_admin
-@loggable
+
 def purge(bot: Bot, update: Update, args: List[str]) -> str:
     msg = update.effective_message  # type: Optional[Message]
     if msg.reply_to_message:
@@ -66,7 +66,7 @@ def purge(bot: Bot, update: Update, args: List[str]) -> str:
 
 @run_async
 @user_admin
-@loggable
+
 def del_message(bot: Bot, update: Update) -> str:
     if update.effective_message.reply_to_message:
         user = update.effective_user  # type: Optional[User]
